fee0ca30d874b699a78bf550c4923260e8ea140c
U_Montag <montagu666@gmail.com>
2024-01-17 11:31:00 +0100

    Create td sign-up page with rud styles via bootstrap

--- src/views/sign-up/SignUp.spec.ts
+++ src/views/sign-up/SignUp.spec.ts
@@ -1,10 +1,105 @@
-import { render, screen } from '@testing-library/vue'
-import SignUp from './SignUp.vue'
+import { render, screen, waitFor } from '@testing-library/vue'
+import userEvent from '@testing-library/user-event'
+import SignUp from '@/views/sign-up/SignUp.vue'
+import { setupServer } from 'msw/node'
+import { HttpResponse, http, type DefaultBodyType } from 'msw'
 
 describe('Sign Up', () => {
   it('has a sign up header', () => {
     render(SignUp)
     const header = screen.getByRole('heading', { name: /sign up/i })
     expect(header).toBeInTheDocument()
+  })
+
+  it('has username input', () => {
+    render(SignUp)
+    expect(screen.getByLabelText('Username')).toBeInTheDocument()
+  })
+
+  it('has email input', () => {
+    render(SignUp)
+    expect(screen.getByLabelText('Email')).toBeInTheDocument()
+  })
+
+  it('has email type for email input', () => {
+    render(SignUp)
+    expect(screen.getByLabelText('Email')).toHaveAttribute('type', 'email')
+  })
+
+  it('has password input', () => {
+    render(SignUp)
+    expect(screen.getByLabelText('Password')).toBeInTheDocument()
+  })
+
+  it('has password type for password input', () => {
+    render(SignUp)
+    expect(screen.getByLabelText('Password')).toHaveAttribute('type', 'password')
+  })
+
+  it('has password repeat input', () => {
+    render(SignUp)
+    expect(screen.getByLabelText('Password Repeat')).toBeInTheDocument()
+  })
+
+  it('has password type for password input', () => {
+    render(SignUp)
+    expect(screen.getByLabelText('Password Repeat')).toHaveAttribute('type', 'password')
+  })
+
+  it('has a sign up button', () => {
+    render(SignUp)
+    const button = screen.getByRole('button', { name: /sign up/i })
+    expect(button).toBeInTheDocument()
+  })
+
+  it('disables sign up button initially', () => {
+    render(SignUp)
+    expect(screen.getByRole('button', { name: /sign up/i })).toBeDisabled()
+  })
+
+  describe('when user sets same value for password inputs', () => {
+    it('enables sign-up button', async () => {
+      const user = userEvent.setup()
+      render(SignUp)
+      const passwordInput = screen.getByLabelText('Password')
+      const passwordRepeatInput = screen.getByLabelText('Password Repeat')
+      await user.type(passwordInput, 'Password')
+      await user.type(passwordRepeatInput, 'Password')
+      expect(screen.getByRole('button', { name: /sign up/i })).toBeEnabled()
+    })
+    describe('when user submits form', () => {
+      it('sends username, email, password to backend', async () => {
+        let requestBody: DefaultBodyType
+        const server = setupServer(
+          http.post('/api/v1/users', async ({ request }) => {
+            requestBody = await request.json()
+            return HttpResponse.json({})
+          })
+        )
+        server.listen()
+        const user = userEvent.setup()
+
+        render(SignUp)
+        const usernameInput = screen.getByLabelText('Username')
+        const emailInput = screen.getByLabelText('Email')
+        const passwordInput = screen.getByLabelText('Password')
+        const passwordRepeatInput = screen.getByLabelText('Password Repeat')
+        await user.type(usernameInput, 'user1')
+        await user.type(emailInput, 'user1@mail.com')
+        await user.type(passwordInput, 'Password')
+        await user.type(passwordRepeatInput, 'Password')
+        const button = screen.getByRole('button', { name: /sign up/i })
+        await user.click(button)
+
+        await waitFor(() =>
+          expect(requestBody).toEqual({
+            username: 'user1',
+            email: 'user1@mail.com',
+            password: 'Password'
+          })
+        )
+        server.close()
+      })
+    })
   })
 })
--- src/views/sign-up/SignUp.vue
+++ src/views/sign-up/SignUp.vue
@@ -1,3 +1,54 @@
+<script setup lang="ts">
+import axios from 'axios';
+import { computed, reactive } from 'vue';
+
+const formState = reactive( {
+  username: '',
+  email: '',
+  password: '',
+  passwordRepeat: ''
+} );
+
+//computed allows to only run into the function when the value changes
+const isDisabled = computed( () => {
+  return formState.password || formState.passwordRepeat
+    ? formState.password !== formState.passwordRepeat
+    : true;
+} );
+
+const onSubmit = () => {
+  const { passwordRepeat: _, ...data } = formState; //destructuring
+  axios.post( ' /api/v1/users', data );
+};
+</script>
+
 <template>
-  <h1>Sign Up</h1>
+  <form @submit.prevent=" onSubmit " class="card">
+    <article class="col-lg-6 offset-lg-3 col-md-8 offset-md-2">
+      <header class="card-header text-center">
+        <h1>Sign Up</h1>
+      </header>
+      <section class="card-body">
+        <div class="mb-3">
+          <label class="form-label" for="username">Username</label>
+          <input class="form-control" type="text" id="username" v-model=" formState.username " />
+        </div>
+        <div class="mb-3">
+          <label class="form-label" for="email">Email</label>
+          <input class="form-control" type="email" id="email" v-model=" formState.email " />
+        </div>
+        <div class="mb-3">
+          <label class="form-label" for="password">Password</label>
+          <input class="form-control" type="password" id="password" v-model=" formState.password " />
+        </div>
+        <div class="mb-3">
+          <label class="form-label" for="passwordRepeat">Password Repeat</label>
+          <input class="form-control" type="password" id="passwordRepeat" v-model=" formState.passwordRepeat " />
+        </div>
+        <div class="text-center">
+          <button class="btn btn-primary" :disabled=" isDisabled " type="submit">Sign Up</button>
+        </div>
+      </section>
+    </article>
+  </form>
 </template>

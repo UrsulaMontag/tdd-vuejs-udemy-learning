814f98f2b26481ddf738a4ba3a09f2bb4e961cc9
U_Montag <montagu666@gmail.com>
2024-01-30 12:30:32 +0100

    Refactor types-file

---
+++ src/shared/types/api-error-types.ts
@@ -0,0 +1,19 @@
+import { AxiosError } from 'axios';
+
+export type FormStateType = {
+    username: string;
+    email: string;
+    password: string;
+    passwordRepeat: string;
+};
+type ValidationErrorsType = {
+    username?: string;
+    email?: string;
+    password?: string;
+    passwordRepeat?: string;
+};
+export type ResponseDataType = {
+    message: string;
+    validationErrors?: ValidationErrorsType;
+};
+export type ErrorsType = ValidationErrorsType | AxiosError;
